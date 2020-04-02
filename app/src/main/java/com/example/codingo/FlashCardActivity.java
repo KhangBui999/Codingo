7b867d6aae7905d3b8fc055043c6338c836d16be
shara <56857006+SgilG91@users.noreply.github.com>
2020-04-02 20:49:59 +1100

    Hardcoded first flashcard

--- app/src/main/java/com/example/codingo/FlashCardActivity.java
+++ app/src/main/java/com/example/codingo/FlashCardActivity.java
@@ -3,13 +3,62 @@ package com.example.codingo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class FlashCardActivity extends AppCompatActivity {
+    private Button card_button;
+    private Button next_button;
+    private TextView text;
+    private TextView tvAnswer;
+    private TextView tvInstructions;
+
+    public FlashCardActivity() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flash_card);
 
+        card_button = (Button) findViewById(R.id.card_button);
+        next_button = (Button) findViewById(R.id.next_button);
+        text = (TextView) findViewById(R.id.text);
+        tvAnswer = (TextView) findViewById(R.id.tvAnswer);
+        tvInstructions = (TextView) findViewById(R.id.tvInstructions);
+
+        final ArrayList<String> questions=new ArrayList<String>();
+        questions.add("Do or not do, there is no try");
+        questions.add("If I have seen further than others, it is by standing upon the shoulders of giants");
+
+        final ArrayList<String> answers=new ArrayList<String>();
+        answers.add("Yoda");
+        answers.add("Newton");
+
+        text.setText(questions.get(0));
+
+        card_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setVisibility(View.INVISIBLE);
+                tvInstructions.setVisibility(View.GONE);
+                tvAnswer.setText(answers.get(0));
+            }
+
+        });
+
+        next_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvAnswer.setVisibility(View.INVISIBLE);
+                text.setVisibility(View.VISIBLE);
+                text.setText(questions.get(1));
+                tvInstructions.setVisibility(View.INVISIBLE);
+            }
+
+        });
     }
 }
--- app/src/main/java/com/example/codingo/MainActivity.java
+++ app/src/main/java/com/example/codingo/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.codingo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    public static final String EXTRA_MESSAGE = "au.edu.unsw.infs3634.beers.MESSAGE";
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_flash_card);
+        Log.i(TAG, "onCreate Called");
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.btnLaunchActivity);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchDetailActivity("Hello from Codingo");
+            }
+        });
+    }
+
+    private void launchDetailActivity(String message) {
+        Intent intent = new Intent(this, FlashCardActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
     }
 }
