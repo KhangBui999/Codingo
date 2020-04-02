140e96a4bdb77fea412b82d91021c2801fa89e4b
Khang <khangb612@gmail.com>
2020-04-03 01:53:10 +1100

    update: completed landing menu page

--- app/src/main/java/com/example/codingo/MainActivity.java
+++ app/src/main/java/com/example/codingo/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.codingo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "codingo.system.message";
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i(TAG, "onCreate called from MainActivity");
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.btn_gs);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchNextActivity();
+            }
+        });
+        Log.i(TAG, "onCreate finished");
+        Log.i(TAG, "MainActivity loaded");
+    }
+
+    private void launchNextActivity() {
+        Log.i(TAG, "launchNextActivity called from MainActivity");
+        Log.i(TAG, "Launching Activity");
+        //Fill in launch for next activity :)
     }
 }
