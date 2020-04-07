d2bc2026fe0774c3d1d45eaa9ea961712ae1b5af
Khang <khangb612@gmail.com>
2020-04-07 18:58:21 +1000

    update: QuizActivity UI completed

--- app/src/main/java/com/example/codingo/Model/Questions.java
+++ app/src/main/java/com/example/codingo/Model/Questions.java
@@ -1,15 +1,21 @@
 package com.example.codingo.Model;
 
+import java.util.ArrayList;
+
 public class Questions {
     private String id;
     private String question;
+    private ArrayList<String> answers; //database attribute will be answer1, answer2, etc...
+    private int correctPosition;
 
     public Questions() {
     }
 
-    public Questions(String id, String question) {
+    public Questions(String id, String question, ArrayList<String> answers, int correctPosition) {
         this.id = id;
         this.question = question;
+        this.answers = answers;
+        this.correctPosition = correctPosition;
     }
 
     public String getId() {
@@ -26,5 +32,21 @@ public class Questions {
 
     public void setQuestion(String question) {
         this.question = question;
+    }
+
+    public ArrayList<String> getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(ArrayList<String> answers) {
+        this.answers = answers;
+    }
+
+    public int getCorrectPosition() {
+        return correctPosition;
+    }
+
+    public void setCorrectPosition(int correctPosition) {
+        this.correctPosition = correctPosition;
     }
 }
