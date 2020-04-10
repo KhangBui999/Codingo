52f5d7d181e6c28aea5a039b1427b6ee9e7916c1
shara <56857006+SgilG91@users.noreply.github.com>
2020-04-10 15:03:14 +1000

    Finished previous/next

--- app/src/main/java/com/example/codingo/FlashCardActivity.java
+++ app/src/main/java/com/example/codingo/FlashCardActivity.java
@@ -6,9 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import com.example.codingo.Model.Flashcard;
-
 import java.util.ArrayList;
 
 public class FlashCardActivity extends AppCompatActivity {
@@ -19,6 +17,7 @@ public class FlashCardActivity extends AppCompatActivity {
 
     private Button card_button;
     private Button next_button;
+    private Button prev_button;
     private TextView text;
     private TextView tvAnswer;
     private TextView tvInstructions;
@@ -31,20 +30,26 @@ public class FlashCardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flash_card);
 
+        //Creates ArrayList including the front and back of each card
         list = new ArrayList<>();
         list.add(new Flashcard("1", "Do or not do, there is no try.", "Yoda"));
         list.add(new Flashcard("2", "If I have seen further than others, it is by " +
                 "standing upon the shoulders of giants", "Newton"));
+        list.add(new Flashcard("3", "So I took the road less traveled by and that " +
+                "has made all the difference.", "Robert Frost"));
         current = list.get(index);
 
         card_button = findViewById(R.id.card_button);
         next_button = findViewById(R.id.next_button);
+        prev_button = findViewById(R.id.prev_button);
         text = findViewById(R.id.text);
         tvAnswer = findViewById(R.id.tvAnswer);
         tvInstructions = findViewById(R.id.tvInstructions);
 
+        //Show the first flashcard
         showFlashcard();
 
+        //Show the answer if the card is clicked
         card_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -53,10 +58,20 @@ public class FlashCardActivity extends AppCompatActivity {
 
         });
 
+        //Show the next flashcard
         next_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 nextFlashcard();
+            }
+
+        });
+
+        //Show the previous flashcard
+        prev_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                prevFlashCard();
             }
 
         });
@@ -67,6 +82,9 @@ public class FlashCardActivity extends AppCompatActivity {
         text.setVisibility(View.VISIBLE);
         tvAnswer.setVisibility(View.GONE);
         tvInstructions.setVisibility(View.VISIBLE);
+        if (index <= 0) {
+            tvInstructions.setText("Click to reveal the answer");
+        }
     }
 
     protected void showAnswer() {
@@ -77,19 +95,25 @@ public class FlashCardActivity extends AppCompatActivity {
     }
 
     protected void nextFlashcard() {
-        if(index < list.size() - 1){
+        if (index < list.size() - 1) {
             index++;
             current = list.get(index);
             showFlashcard();
+        } else {
+            tvInstructions.setText("You have reached the last flashcard! Good job!");
+            index = -1;
+            showFlashcard();
         }
-        else {
-            tvInstructions.setText("You have reached the end! Good job!");
-            text.setVisibility(View.GONE);
-            tvAnswer.setVisibility(View.GONE);
-            tvInstructions.setVisibility(View.VISIBLE);
-            card_button.setOnClickListener(null);
-            next_button.setOnClickListener(null);
-            //Include a back button or something later on :)
+    }
+
+    protected void prevFlashCard() {
+        if (index > 0) {
+            index--;
+            current = list.get(index);
+            showFlashcard();
+        } else {
+            index = list.size();
+            showFlashcard();
         }
     }
 
