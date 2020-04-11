635d2568aeb677808021051c6da4eebb6d5bd45f
Khang <khangb612@gmail.com>
2020-04-11 16:07:52 +1000

    chore: cleaned up video activity for merge

--- app/src/main/java/com/example/codingo/VideoActivity.java
+++ app/src/main/java/com/example/codingo/VideoActivity.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.widget.TextView;
 
 
+import com.example.codingo.Utilities.BottomNavHelper;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
@@ -19,6 +21,7 @@ public class VideoActivity extends YouTubeBaseActivity {
     private TextView mTopic;
     private TextView mContent;
     private TextView mPrompt;
+    private BottomNavigationView mNavigation;
     private Content mCon;
     private static final String API_KEY = "AIzaSyB-X0GINXGVnrcwiFAMx9bZliAvMSEcEu0";
     // initialised variables for the youtube player
@@ -31,16 +34,20 @@ public class VideoActivity extends YouTubeBaseActivity {
         Intent intent = getIntent();
         int position = intent.getIntExtra(MainActivity.EXTRA_MESSAGE, 0);
         ArrayList<Content> contents = Content.getContents();
-        // retrieving information from the contents class
-        mCon=contents.get(position);
-        mPrompt= (TextView) findViewById(R.id.prompt);
-        mView = (YouTubePlayerView) findViewById(R.id.view);
-        mTopic = (TextView) findViewById(R.id.topic);
-        mContent = (TextView) findViewById(R.id.content);
 
+        BottomNavHelper bnh = new BottomNavHelper();
+        mNavigation = bnh.getNavMenu(this, R.id.navigation);
+
+        // Retrieving information from the contents class
+        mCon = contents.get(position);
+        mPrompt= findViewById(R.id.prompt);
+        mView = findViewById(R.id.view);
+        mTopic =  findViewById(R.id.topic);
+        mContent = findViewById(R.id.content);
+
+        // Changes text based on content
         mTopic.setText(mCon.getTopic());
         mContent.setText(mCon.getContent());
-        // changes text based on content
 
         mListener = new YouTubePlayer.OnInitializedListener() {
             @Override
@@ -50,10 +57,11 @@ public class VideoActivity extends YouTubeBaseActivity {
             //Interface definition for callbacks that are invoked when player initialization succeeds or fails
             @Override
             public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
-                mPrompt.setText("This activity_video is not available!");
+                mPrompt.setText("This video is currently not available!");
             }
         };
-        // When clicking the Youtube player, the youtube player will load
+
+        // When clicking the Youtube player, the youtube video will load
         mView.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick (View view){
